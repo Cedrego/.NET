25a3d255dee1ba99a5aaeaf51862be23d82e29a4
Snorlax555 <cami.n.molina@gmail.com>
2025-10-24 22:24:59 -0300

    Crear Cuenta + Cambios en Corredor.cs

--- LaboratorioNET/Entities/Corredor.cs
+++ LaboratorioNET/Entities/Corredor.cs
@@ -14,10 +14,16 @@ namespace LaboratorioNET.Entities
         [FirestoreProperty("nombre")]
         public string Nombre { get; set; } = string.Empty;
 
-        [FirestoreProperty("origen")]
-        public string Origen { get; set; } = string.Empty;
+        [FirestoreProperty("correo")]
+        public string Correo { get; set; } = string.Empty;
 
-        [FirestoreProperty("telefono")]
-        public string Telefono { get; set; } = string.Empty;
+        [FirestoreProperty("contraseña")]
+        public string Contraseña { get; set; } = string.Empty;
+
+        [FirestoreProperty("fechaNacimiento")]
+        public string FechaNacimiento { get; set; } = string.Empty;
+
+        [FirestoreProperty("nacionalidad")]
+        public string Nacionalidad { get; set; } = string.Empty;
     }
 }
